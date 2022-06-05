7fcec6c02e387cfafeb153f9fd90139c708183f8
vhqa2005 <bapbenbin1997@gmail.com>
2022-06-05 19:17:39 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Hí Chào Cậu 🥳🥳 ',
+    introDesc: `Tớ có điều này muốn hỏi cậu, Cậu nhớ phải trả lời thật lòng đó nha :33`,
+    btnIntro: 'Ucii',
+    title: 'Cậu thấy tớ như thế nào?? 😙',
+    desc: 'Nếu cậu không trả lời hoặc thoát ra, tức là cậu đồng ý làm người iu của tớ đó nha :))) ',
+    btnYes: 'Cậu đáng iu vlllll <3 <3 <3',
+    btnNo: 'Hong biếc nữa :((',
+    question:'Vì sao cậu không ib zới tớ ☹️☹️ ',
+    reasonPlaceholder: 'Tại seooo',
+    btnReply: 'Gữii 💌💌',
+    reply: 'Tại vì tớ thích cậu đó Ahiiiii 😊😊',
+    mess: 'Tớ biết mà 🥰. Love you 3.000 😘😘',
+    messDesc: 'Tối nay cậu dành thời gian nhắn tin zới tớ nha😘😘',
+    btnAccept: 'Ucii lunn 💖',
+    messLink: 'https://www.facebook.com/messages/t/I.AM.VHQA/'
+}
